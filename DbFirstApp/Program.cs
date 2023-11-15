8f98f3b89468fe4d7afb7a4b0767725213c2b438
Egor Forinov <forinov22@gmail.com>
2023-11-15 10:59:46 +0300

    Modified to asp.net core app, added brands controller

--- DbFirstApp/Program.cs
+++ DbFirstApp/Program.cs
@@ -1,43 +1,52 @@
-﻿
-using DbFirstApp.Data;
+﻿using DbFirstApp.Data;
 using DbFirstApp.Models;
 using Microsoft.EntityFrameworkCore;
+using OnlineShop;
 
-var context = new OnlineShopContext();
-context.Database.Migrate();
-
-async Task<IEnumerable<Product>> GetAllProductsByBrand(OnlineShopContext ctx, string brandName)
+public class Program
 {
-    var brand = await ctx.Brands.FirstAsync(b => b.Name == brandName);
-    var products = await ctx.Products.Where(p => p.BrandId == brand.Id).ToListAsync();
-    return products;
-}
-
-async Task<IEnumerable<ProductVersion>> GetAllProductVersionsByProduct(OnlineShopContext ctx, string productName)
-{
-    var product = await ctx.Products.FirstAsync(p => p.Name == productName);
-    var productVersions = await ctx.ProductVersions.Where(pv => pv.ProductId == product.Id).ToListAsync();
-    return productVersions;
-}
-
-async Task<IDictionary<Brand, int>> GetAllBrandsProductCount(OnlineShopContext ctx)
-{
-    var brands = await ctx.Brands.Include(b => b.Products).ToListAsync();
-    var d = new Dictionary<Brand, int>();
-    foreach (var brand in brands)
+    static void Main(string[] args)
     {
-        d.Add(brand, brand.Products.Count);
+        CreateHostBuilder(args).Build().Run();
     }
-    return d;
-}
 
-async Task<IEnumerable<Product>> GetAllProductsBySectionAndCategory(OnlineShopContext ctx, string sectionName, string categoryName)
-{
-    var section = await ctx.Sections.FirstAsync(s => s.Name == sectionName);
-    var category = await ctx.Categories.Include(c => c.Sections).FirstAsync(c => c.Name == categoryName);
+    async Task<IEnumerable<Product>> GetAllProductsByBrand(OnlineShopContext ctx, int brandId)
+    {
+        var products = await ctx.Products.Where(p => p.BrandId == brandId).ToListAsync();
+        return products;
+    }
 
-    var products = await ctx.Products.Where(p => p.CategoryId == category.Id && category.Sections.Contains(section)).ToListAsync();
-    return products;
-}
+    async Task<IEnumerable<ProductVersion>> GetAllProductVersionsByProduct(OnlineShopContext ctx, int productId)
+    {
+        var productVersions = await ctx.ProductVersions.Where(pv => pv.ProductId == productId).ToListAsync();
+        return productVersions;
+    }
 
-    
+    async Task<IDictionary<Brand, int>> GetAllBrandsProductCount(OnlineShopContext ctx)
+    {
+        var brands = await ctx.Brands.Include(b => b.Products).ToListAsync();
+        var d = new Dictionary<Brand, int>();
+        foreach (var brand in brands)
+        {
+            d.Add(brand, brand.Products.Count);
+        }
+        return d;
+    }
+
+    async Task<IEnumerable<Product>> GetAllProductsBySectionAndCategory(OnlineShopContext ctx, int sectionId, int categoryId)
+    {
+        var products = ctx.Products
+            .Where(p => p.CategoryId == categoryId)
+            .Include(p => p.Category.Sections);
+
+        var section = products.First().Category.Sections.Single(s => s.Id == sectionId);
+        return section == null ? Enumerable.Empty<Product>() : products;
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
+}; 
---
+++ DbFirstApp/Startup.cs
@@ -0,0 +1,22 @@
+﻿using DbFirstApp.Data;
+
+namespace OnlineShop
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<OnlineShopContext>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
